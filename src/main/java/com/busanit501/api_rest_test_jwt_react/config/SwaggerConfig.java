11ca4204e6e05845458bffeba6a7bddc07093152
이상용 <lsy3709@naver.com>
2025-08-21 10:43:45 +0900

    2교시, 스프링 부트 설정 작업4, 임시 확인2

--- src/main/java/com/busanit501/api_rest_test_jwt_react/config/SwaggerConfig.java
+++ src/main/java/com/busanit501/api_rest_test_jwt_react/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.busanit501.api_rest_test_jwt_react.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
