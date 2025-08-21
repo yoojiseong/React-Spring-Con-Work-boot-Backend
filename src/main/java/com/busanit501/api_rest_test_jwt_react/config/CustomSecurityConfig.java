b378bdd397a34dbb65b4900974aa471a92d21f55
이상용 <lsy3709@naver.com>
2025-08-21 10:30:58 +0900

    2교시, 스프링 부트 설정 작업3, config 설정 파일 작업중

---
+++ src/main/java/com/busanit501/api_rest_test_jwt_react/config/CustomSecurityConfig.java
@@ -0,0 +1,125 @@
+package com.busanit501.api_rest_test_jwt_react.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+
+// 작업 순서1
+@Log4j2
+@Configuration
+// 어노테이션을 이용해서, 특정 권한 있는 페이지 접근시, 구분가능.
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+// 위 어노테이션 지원중단, 아래 어노테이션 으로 교체, 기본으로 prePostEnabled = true ,
+@EnableMethodSecurity
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+    //추가 1-1
+    private final APIUserDetailsService apiUserDetailsService;
+    private final JWTUtil jwtUtil;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("시큐리티 동작 확인 ====webSecurityCustomizer======================");
+        return (web) ->
+                web.ignoring()
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.info("===========config=================");
+
+        // 인증 관련된 설정을 하는 도구.
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+                // 우리 코드에서 로그인을 담당하는 도구 옵션 추가.
+                .userDetailsService(apiUserDetailsService)
+                // 평문 암호화 해주는 도구 옵션 추가.
+                .passwordEncoder(passwordEncoder());
+
+        // Get AuthenticationManager 세팅1
+        AuthenticationManager authenticationManager =
+                authenticationManagerBuilder.build();
+
+        //반드시 필요 세팅1
+        // 적용하기.
+        http.authenticationManager(authenticationManager);
+
+        //APILoginFilter 세팅1
+        // 아이디:mid- lsy, 패스워드: mpw- 1234 첨부해서,
+        // localhost:8080/generateToken
+        // 디비 등록된 유저에 대해서만, 토큰 발급.
+
+        APILoginFilter apiLoginFilter = new APILoginFilter("/generateToken");
+        apiLoginFilter.setAuthenticationManager(authenticationManager);
+
+        // APILoginSuccessHandler 생성: 인증 성공 후 처리 로직을 담당
+        APILoginSuccessHandler successHandler = new APILoginSuccessHandler(jwtUtil);
+
+// SuccessHandler 설정: 로그인 성공 시 APILoginSuccessHandler가 호출되도록 설정
+        apiLoginFilter.setAuthenticationSuccessHandler(successHandler);
+
+        //APILoginFilter의 위치 조정 세팅1, 사용자 인증 전에 ,
+        http.addFilterBefore(apiLoginFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // /api 경로에 대해 TokenCheckFilter 적용
+        http.addFilterBefore(
+                tokenCheckFilter(jwtUtil,apiUserDetailsService),
+                UsernamePasswordAuthenticationFilter.class
+        );
+
+        // RefreshTokenFilter를 TokenCheckFilter 이전에 등록
+        http.addFilterBefore(
+                new RefreshTokenFilter("/refreshToken", jwtUtil),
+                TokenCheckFilter.class
+        );
+        //cors 정책 설정
+        http.cors(httpSecurityCorsConfigurer ->
+                httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource())
+        );
+        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+    private TokenCheckFilter tokenCheckFilter(JWTUtil jwtUtil, APIUserDetailsService apiUserDetailsService){
+        return new TokenCheckFilter(apiUserDetailsService, jwtUtil);
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 모두 허용 , 리액트 예, Nginx, http://localhost:80
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+//        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:80"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
